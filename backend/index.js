9d3cc7152f9bcdac169c648ecc0ac251387debd3
Jeremie-Allemand <jeremie.allemand@ceff.ch>
2022-02-28 15:21:29 +0100

    import du fichier

--- backend/index.js
+++ backend/index.js
@@ -11,15 +11,46 @@ const io = require('socket.io')(http,{
 })
 
 const PORT = process.env.PORT || 3001
-
+var loaded = false
 app.use(express.json());
 app.use(cors())
 global.players = []
+global.familles = []
 global.questions = []
 global.socket_io = io
 
 require('./routes/game.route')(app)
 require('./routes/player.route')(app)
+
+
+const importCSV = () => {
+  const fs = require('fs')
+  fs.readFile('questions.csv', 'utf-8' , (err, file) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    console.log(file)
+    const rows = file.toString().trim().split('\r\n')
+    var splited = []
+    rows.forEach((item) => {
+      splited.push(item.split(';'))
+    })
+    splited.filter(obj => !(obj && Object.keys(obj).length === 0))
+    splited.forEach((item) => {
+      if(item[0].charAt(0) == '$'){
+        item[0] = item[0].substring(1)
+        global.questions.push(item)
+      }
+      if(item[0].charAt(0) == '#'){
+        item[0] = item[0].substring(1)
+        global.familles.push(item)
+      }
+    })
+  })
+}
+
+importCSV()
 
 http.listen(PORT, () =>
   console.log(`Express server is running on localhost:${PORT}`)
